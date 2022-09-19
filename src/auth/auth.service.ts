9761e3b0bdcf2ef9019abcd123d8a39a92f32691
LAPTOP-SEEN5R81\qudtj <qudtjr9849@gmail.com>
2022-09-19 16:49:55 +0900

    Create jwt module and function

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -3,24 +3,33 @@ import { InjectRepository } from '@nestjs/typeorm';
 import { AuthCredentialsDto } from './dto/auth-credential.dto';
 import { UserRepository } from './user.repository';
 import * as bcrypt from 'bcryptjs';
+import { JwtService } from '@nestjs/jwt';
+import { User } from './user.entity';
 
 @Injectable()
 export class AuthService {
   constructor(
     @InjectRepository(UserRepository)
     private userRepository: UserRepository,
+    private jwtService: JwtService,
   ) {}
 
   async signUp(authCredentialsDto: AuthCredentialsDto): Promise<void> {
     return this.userRepository.createUser(authCredentialsDto);
   }
 
-  async singIn(authCredentialsDto: AuthCredentialsDto): Promise<string> {
+  async singIn(
+    authCredentialsDto: AuthCredentialsDto,
+  ): Promise<{ accessToken: string }> {
     const { username, password } = authCredentialsDto;
     const user = await this.userRepository.findOne({ username });
 
     if (user && (await bcrypt.compare(password, user.password))) {
-      return 'logIn success';
+      // 유저 토큰을 생성해준다. (secret + payload)
+      const payload = { username };
+      const accessToken = await this.jwtService.sign(payload);
+
+      return { accessToken };
     } else {
       throw new UnauthorizedException('logIn failed');
     }
